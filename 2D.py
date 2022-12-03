9d9f8b4daf84fe453102737b5185cacf06dc659a
tzoli6 <zozoturi@gmail.com>
2022-12-03 16:54:54 +0100

    "

--- 2D.py
+++ 2D.py
@@ -2,7 +2,13 @@ import numpy as np
 from matplotlib import pyplot as plt
 import pandas as pd
 import seaborn as sns
-sns.set_theme(style="darkgrid")
 
-df = pd.read_csv('corr_test.txt')
+sns.set_theme()
+
+df = pd.read_csv('G:\My Drive\TU Delft\LowSpeedWindTunnelTest\LSWTT-Group16\Measurement_Data\\twoD\main.csv',skipinitialspace=True)
+#df=df.drop(labels=0)
 print(df)
+sns.scatterplot(data=df, x='Alpha' ,y='Cl')
+sns.relplot(data=df, x='Alpha' ,y='Cl',kind='line')
+
+plt.show()
--- 3D.py
+++ 3D.py
@@ -1,2 +1,12 @@
-import sys
-print(sys.path)
+import numpy as np
+from matplotlib import pyplot as plt
+import pandas as pd
+import seaborn as sns
+
+sns.set_theme()
+
+df = pd.read_csv('G:\My Drive\TU Delft\LowSpeedWindTunnelTest\LSWTT-Group16\Measurement_Data\D\corr_test.csv')
+print(df)
+sns.relplot(data=df, x='Alpha' ,y='CL',markers='O')
+#plt.plot(df['Alpha-pr'],df['Cl-press'])
+plt.show()
