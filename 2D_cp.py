5b2747af091a6e94c63e1af81b8f93653f898b01
tzoli6 <zozoturi@gmail.com>
2022-12-03 18:43:03 +0100

    d

--- 2D_cp.py
+++ 2D_cp.py
@@ -4,7 +4,7 @@ import pandas as pd
 import seaborn as sns
 
 sns.set_theme()
-
+sns.set_style('darkgrid')
 df = pd.read_csv('G:\My Drive\TU Delft\LowSpeedWindTunnelTest\LSWTT-Group16\Measurement_Data\\twoD\cp_data.csv',skipinitialspace=True)
 df = df.T
 print(df)
@@ -12,7 +12,8 @@ print(df[1])
 for i in range(1,43):
     #sns.scatterplot(data=df, x='Alpha' ,y='Cl')
     #sns.relplot(data=df, x='Alpha' ,y='Cl',kind='line')
-    sns.scatterplot(data=df, x=df[0], y=df[i])
-    plt.plot(df[0], df[i])
-    plt.savefig('G:\My Drive\TU Delft\LowSpeedWindTunnelTest\LSWTT-Group16\Measurement_Data\\twoD\Graphs\\'+str(df[i]['Alpha'])+'.jpg')
+    plt.plot(df[0], df[i], color='grey',linewidth='1')
+    f=sns.scatterplot(data=df, x=df[0], y=df[i], marker =(4,1,0), color='seagreen')
+    f.set(xlabel='Alpha [deg]', ylabel='Cp')
+    plt.savefig('G:\My Drive\TU Delft\LowSpeedWindTunnelTest\LSWTT-Group16\Measurement_Data\\twoD\Graphs\\'+str(df[i]['Alpha'])+'.svg',format='svg', dpi=1200)
     plt.clf()
