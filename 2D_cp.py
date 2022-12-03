fb6b0bebce7c8e406c0e904306c0109a80d7417a
tzoli6 <zozoturi@gmail.com>
2022-12-03 18:14:45 +0100

    Cp mainly done

---
+++ 2D_cp.py
@@ -0,0 +1,18 @@
+import numpy as np
+from matplotlib import pyplot as plt
+import pandas as pd
+import seaborn as sns
+
+sns.set_theme()
+
+df = pd.read_csv('G:\My Drive\TU Delft\LowSpeedWindTunnelTest\LSWTT-Group16\Measurement_Data\\twoD\cp_data.csv',skipinitialspace=True)
+df = df.T
+print(df)
+print(df[1])
+for i in range(1,43):
+    #sns.scatterplot(data=df, x='Alpha' ,y='Cl')
+    #sns.relplot(data=df, x='Alpha' ,y='Cl',kind='line')
+    sns.scatterplot(data=df, x=df[0], y=df[i])
+    plt.plot(df[0], df[i])
+    plt.savefig('G:\My Drive\TU Delft\LowSpeedWindTunnelTest\LSWTT-Group16\Measurement_Data\\twoD\Graphs\\'+str(df[i]['Alpha'])+'.jpg')
+    plt.clf()
