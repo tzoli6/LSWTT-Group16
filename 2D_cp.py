91b3c2392fb4f89131390c442d5e41a7a467b58e
Barnabas Tasi <barnab.tasi03@gmail.com>
2022-12-03 18:55:14 +0100

    Update 2D_cp.py

--- 2D_cp.py
+++ 2D_cp.py
@@ -5,7 +5,8 @@ import seaborn as sns
 
 sns.set_theme()
 
-df = pd.read_csv('G:\My Drive\TU Delft\LowSpeedWindTunnelTest\LSWTT-Group16\Measurement_Data\\twoD\cp_data.csv',skipinitialspace=True)
+df = pd.read_csv('.\Measurement_Data\\twoD\cp_data.csv',skipinitialspace=True)
+#df = pd.read_csv('G:\My Drive\TU Delft\LowSpeedWindTunnelTest\LSWTT-Group16\Measurement_Data\\twoD\cp_data.csv',skipinitialspace=True)
 df = df.T
 print(df)
 print(df[1])
@@ -14,5 +15,6 @@ for i in range(1,43):
     #sns.relplot(data=df, x='Alpha' ,y='Cl',kind='line')
     sns.scatterplot(data=df, x=df[0], y=df[i])
     plt.plot(df[0], df[i])
-    plt.savefig('G:\My Drive\TU Delft\LowSpeedWindTunnelTest\LSWTT-Group16\Measurement_Data\\twoD\Graphs\\'+str(df[i]['Alpha'])+'.jpg')
+    plt.savefig('.\Measurement_Data\\twoD\Graphs\\'+str(df[i]['Alpha'])+'.jpg')
+    #plt.savefig('G:\My Drive\TU Delft\LowSpeedWindTunnelTest\LSWTT-Group16\Measurement_Data\\twoD\Graphs\\'+str(df[i]['Alpha'])+'.jpg')
     plt.clf()
