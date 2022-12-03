a5f30690b709249435896f28dc90744c0b516f2e
tzoli6 <zozoturi@gmail.com>
2022-12-03 14:13:40 +0100

    1

--- 2D.py
+++ 2D.py
@@ -1,5 +1,8 @@
 import numpy as np
-from matplotlib import plotly as plt
+from matplotlib import pyplot as plt
 import pandas as pd
 import seaborn as sns
 sns.set_theme(style="darkgrid")
+
+df = pd.read_csv('corr_test.txt')
+print(df)
