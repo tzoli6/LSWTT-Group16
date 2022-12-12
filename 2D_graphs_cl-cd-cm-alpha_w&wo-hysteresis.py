e20414d1815559ae71b5e450f685ef149a276ba5
CampingTurret <l.gaeta@student.tudelft.nl>
2022-12-12 06:37:39 +0100

    2d graphs with or without hysteresis

---
+++ 2D_graphs_cl-cd-cm-alpha_w&wo-hysteresis.py
@@ -0,0 +1,48 @@
+from cProfile import label
+import numpy as np
+from matplotlib import pyplot as plt
+import pandas as pd
+import seaborn as sns
+
+sns.set_theme()
+sns.set_style('darkgrid')
+df = pd.read_csv('.\Measurement_Data\\twoD\main.csv',skipinitialspace=True)
+#df = pd.read_csv('G:\My Drive\TU Delft\LowSpeedWindTunnelTest\LSWTT-Group16\Measurement_Data\\twoD\cp_data.csv',skipinitialspace=True)
+df = pd.DataFrame(df)
+print(df)
+df = df[['Alpha','Cl','Cm','Cd']]
+da = df[:33]
+print(df)
+plt.plot(df['Alpha'], df['Cl'], color='red',linewidth='1', label = 'Cl')
+plt.plot(df['Alpha'], df['Cm'], color='gray',linewidth='1', label = 'Cm')
+plt.legend
+f=sns.scatterplot(data=df, x=df['Alpha'], y=df['Cl'], marker ='X', color='seagreen')
+f.set(xlabel='Alpha [degrees]', ylabel='Cl/Cm [-]')
+plt.savefig('.\Measurement_Data\\twoD\Graphs\\'+str(['ClCm-Alpha'])+'.svg',dpi=1200)
+    #plt.savefig('G:\My Drive\TU Delft\LowSpeedWindTunnelTest\LSWTT-Group16\Measurement_Data\\twoD\Graphs\\'+str(df[i]['Alpha'])+'.svg',format='svg', dpi=1200)
+plt.clf()
+
+plt.plot(da['Alpha'], da['Cl'], color='red',linewidth='1',label = 'Cl')
+plt.plot(da['Alpha'], da['Cm'], color='gray',linewidth='1',label = 'Cm')
+plt.legend
+q=sns.scatterplot(data=da, x=da['Alpha'], y=da['Cl'], marker ='X', color='seagreen')
+q.set(xlabel='Alpha [degrees]', ylabel='Cl/Cm [-]')
+plt.savefig('.\Measurement_Data\\twoD\Graphs\\'+str(['ClCm-Alpha_nohysteresis'])+'.svg',dpi=1200)
+    #plt.savefig('G:\My Drive\TU Delft\LowSpeedWindTunnelTest\LSWTT-Group16\Measurement_Data\\twoD\Graphs\\'+str(df[i]['Alpha'])+'.svg',format='svg', dpi=1200)
+plt.clf()
+
+plt.plot(df['Cl'], df['Cd'], color='red',linewidth='1')
+plt.legend
+f=sns.scatterplot(data=df, x=df['Cl'], y=df['Cd'], marker ='X', color='seagreen')
+f.set(xlabel='Cl [-]', ylabel='Cd [-]')
+plt.savefig('.\Measurement_Data\\twoD\Graphs\\'+str(['Cl-Cd'])+'.svg',dpi=1200)
+    #plt.savefig('G:\My Drive\TU Delft\LowSpeedWindTunnelTest\LSWTT-Group16\Measurement_Data\\twoD\Graphs\\'+str(df[i]['Alpha'])+'.svg',format='svg', dpi=1200)
+plt.clf()
+
+plt.plot(da['Cl'], da['Cd'], color='red',linewidth='1')
+plt.legend
+q=sns.scatterplot(data=da, x=da['Cl'], y=da['Cd'], marker ='X', color='seagreen')
+q.set(xlabel='Cl [-]', ylabel='Cd [-]')
+plt.savefig('.\Measurement_Data\\twoD\Graphs\\'+str(['Cl-Cd_nohysteresis'])+'.svg',dpi=1200)
+    #plt.savefig('G:\My Drive\TU Delft\LowSpeedWindTunnelTest\LSWTT-Group16\Measurement_Data\\twoD\Graphs\\'+str(df[i]['Alpha'])+'.svg',format='svg', dpi=1200)
+plt.clf()
