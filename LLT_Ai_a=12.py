9a9dbef9489d49c70e33d249b64fe187e79696c9
JimmyWJChen <W.J.Chen-1@student.tudelft.nl>
2022-12-16 12:42:53 +0100

    Added plots of the induced angle

---
+++ LLT_Ai_a=12.py
@@ -0,0 +1,14 @@
+import numpy as np
+import seaborn as sns
+import pandas as pd
+from matplotlib import pyplot as plt
+
+sns.set_theme()
+
+
+
+df = pd.read_csv(".\CFD\\3D\\3Dwindtunnelmodel_a=12.00_v=49.00ms_induced_angle.csv", skipinitialspace=True, delimiter=';')
+
+sns.relplot(data=df, x='y-span', y='Ai', markers='o')
+plt.show()
+print(df)
