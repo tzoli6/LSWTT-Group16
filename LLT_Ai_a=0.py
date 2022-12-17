0a8acf6ad799270416ab5cf3abeab329f0baf40f
JimmyWJChen <W.J.Chen-1@student.tudelft.nl>
2022-12-17 18:12:12 +0100

    The plots now get saved as a .svg file.

--- LLT_Ai_a=0.py
+++ LLT_Ai_a=0.py
@@ -4,11 +4,18 @@ import pandas as pd
 from matplotlib import pyplot as plt
 
 sns.set_theme()
-
+sns.set_style('darkgrid')
 
 
 df = pd.read_csv(".\CFD\\3D\\3Dwindtunnelmodel_a=0.00_v=49.00ms_induced_angle.csv", skipinitialspace=True, delimiter=';')
 
-sns.relplot(data=df, x='y-span', y='Ai', markers='o')
+data = pd.DataFrame(df)
+data_vector = data[['y-span', 'Ai']]
+plt.plot(data_vector['y-span'], data_vector['Ai'], color='grey',linewidth='1')
+
+f = sns.relplot(data=df, x='y-span', y='Ai', markers='o')
+f.set(xlabel='y-span [m]', ylabel='Ai [degrees]')
 plt.show()
 print(df)
+plt.savefig('.\CFD\\3D\graphs\\'+str(['induced_angle-alpha=0'])+'.svg',dpi=1200)
+plt.clf()
