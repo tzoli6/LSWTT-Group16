1040435ec0fcfaefc36b7785eb0f689cca564cfd
tzoli6 <zozoturi@gmail.com>
2022-12-18 14:13:53 +0100

    f

--- 2D_CFD.py
+++ 2D_CFD.py
@@ -2,14 +2,122 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 import seaborn.objects as so
 import pandas as pd
+import numpy as np
 
-#sns.set_theme()
-#sns.set_style('darkgrid')
+
+
+sns.set_theme()
+sns.set_style('darkgrid', {'axes.facecolor':'.9'})
 
 df = pd.read_csv('.\\CFD\\2D\data.csv', delimiter=',', skipinitialspace=True)
 df.drop([0], inplace=True)
+df = df.astype(float)
 
-#f = so.Plot(data = df, x='alpha', y='CL')
-#f.add(so.Dot())
-plt.plot(df.loc[:,'alpha'], df.loc[:,'CD'])
-plt.show()
+df_data = pd.read_csv('.\Measurement_Data\\twoD\main.csv',skipinitialspace=True)
+
+df_con= pd.concat([df.assign(dataset='CFD'), df_data.assign(dataset='Measurement')])
+print(df)
+sns.set(rc={'figure.figsize':(6,16)})
+
+#sns.relplot(df, x='alpha', y='CL', style='dataset', kind='line', color='grey', legend=False)
+#sns.scatterplot(df, x='alpha', y='CL', style='dataset')
+#sns.relplot(df_data, x='Alpha', y='Cl', kind='line', color='grey')
+#sns.scatterplot(df_data, x= 'Alpha', y='Cl')
+def save_cfd_plots():
+    #Cl-alpha
+    f=sns.relplot(df, x='alpha', y='CL', kind='line', color='grey', linewidth='1', aspect=16/11)
+    sns.scatterplot(df, x='alpha', y='CL', markers='x', color='seagreen')
+    f.set(xlabel='\u03B1 [\u00B0]', ylabel='Cl [-]')
+    plt.yticks(np.arange(-1,1.5,0.25))
+    plt.xticks(np.arange(-10,20,2.5))
+    #plt.savefig('.\CFD\\2D\Graphs\CFD_Cl-alpha.svg',dpi=1200)
+    plt.clf()
+    #cd-alpha
+    f=sns.relplot(df, x='alpha', y='CD', kind='line', color='grey', linewidth='1', aspect=16/11)
+    sns.scatterplot(df, x='alpha', y='CD', markers='x', color='seagreen')
+    f.set(xlabel='\u03B1 [\u00B0]', ylabel='Cd [-]')
+    plt.yticks(np.arange(-0.05,0.15,0.02))
+    plt.xticks(np.arange(-10,20,2.5))
+    #plt.savefig('.\CFD\\2D\Graphs\CFD_Cd-alpha.svg',dpi=1200)
+    plt.clf()
+    #cd-cl
+    plt.plot(df.loc[:,'CD'], df.loc[:,'CL'], color='grey',linewidth='1')
+    f= sns.scatterplot(df, x='CD', y='CL', markers='x', color='seagreen')
+    f.set(xlabel='Cd [-]', ylabel='Cl [-]')
+    plt.yticks(np.arange(-1,1.5,0.2))
+    plt.xticks(np.arange(-0.05,0.15,0.02))
+    #plt.savefig('.\CFD\\2D\Graphs\CFD_Cl-Cd.svg',dpi=1200)
+    plt.clf()
+    #cm-alpha
+    f=sns.relplot(df, x='alpha', y='Cm', kind='line', color='grey', linewidth='1', aspect=16/11)
+    sns.scatterplot(df, x='alpha', y='Cm', markers='x', color='seagreen')
+    f.set(xlabel='\u03B1 [\u00B0]', ylabel='Cm [-]')
+    plt.yticks(np.arange(-0.03,0.06,0.01))
+    plt.xticks(np.arange(-10,22.5,2.5))
+    plt.savefig('.\CFD\\2D\Graphs\CFD_Cm-alpha.svg',dpi=1200)
+    plt.clf()
+
+def save_comperison_plots():
+    sns.set_palette('Paired')
+    #cl-alpha
+    g=sns.relplot(df, x='alpha', y='CL', kind='line', linewidth='1',color='grey', aspect=16/11)
+    sns.scatterplot(df_con, x='alpha', y='CL', style='dataset', hue='dataset')
+    g.axes.flat[0].get_legend().set_title('')
+    plt.plot(df_data.loc[:,'alpha'], df_data.loc[:,'CL'], color='grey',linewidth='1')
+    g.set(xlabel='\u03B1 [\u00B0]', ylabel='Cl [-]')
+    plt.yticks(np.arange(-1,1.5,0.25))
+    plt.xticks(np.arange(-10,22.5,2.5))
+    plt.savefig('.\CFD\\2D\Graphs\CFD_M_Cl-alpha.svg',dpi=1200)
+    plt.clf()
+    #Cd-alpha
+    g=sns.relplot(df, x='alpha', y='CD', kind='line', linewidth='1',color='grey', aspect=16/11)
+    sns.scatterplot(df_con, x='alpha', y='CD', style='dataset', hue='dataset')
+    g.axes.flat[0].get_legend().set_title('')
+    plt.plot(df_data.loc[:,'alpha'], df_data.loc[:,'CD'], color='grey',linewidth='1')
+    g.set(xlabel='\u03B1 [\u00B0]', ylabel='Cd [-]')
+    plt.yticks(np.arange(-0.05,0.35,0.05))
+    plt.xticks(np.arange(-10,20,2.5))
+    plt.savefig('.\CFD\\2D\Graphs\CFD_M_Cd-alpha.svg',dpi=1200)
+    plt.clf()
+
+    #Cd-Cl
+    g=sns.relplot(df_con, x='CD', y='CL',kind='scatter', style='dataset', hue='dataset', aspect=16/11,legend=False)
+    sns.scatterplot(df_con, x='CD', y='CL', style='dataset', hue='dataset')
+    g.axes.flat[0].get_legend().set_title('')
+    plt.plot(df_data.loc[:,'CD'], df_data.loc[:,'CL'], color='grey',linewidth='1')
+    plt.plot(df.loc[:,'CD'], df.loc[:,'CL'], color='grey',linewidth='1')
+    g.set(xlabel='Cl [-]', ylabel='Cd [-]')
+    plt.yticks(np.arange(-1,1.5,0.25))
+    plt.xticks(np.arange(-0.05,0.35,0.05))
+    plt.savefig('.\CFD\\2D\Graphs\CFD_M_Cd-Cl.svg',dpi=1200)
+    plt.clf()
+#Cm-alpha
+    g=sns.relplot(df, x='alpha', y='Cm', kind='line', linewidth='1',color='grey', aspect=16/11)
+    sns.scatterplot(df_con, x='alpha', y='Cm', style='dataset', hue='dataset')
+    g.axes.flat[0].get_legend().set_title('')
+    plt.plot(df_data.loc[:,'alpha'], df_data.loc[:,'Cm'], color='grey',linewidth='1')
+    g.set(xlabel='\u03B1 [\u00B0]', ylabel='Cm [-]')
+    plt.yticks(np.arange(-0.1,0.06,0.01))
+    plt.xticks(np.arange(-10,22.5,2.5))
+    plt.savefig('.\CFD\\2D\Graphs\CFD_M_Cm-alpha.svg',dpi=1200)
+    plt.clf()
+
+def main():
+    #save_cfd_plots()
+    save_comperison_plots()
+
+
+
+'''
+f = so.Plot(data = df, x='alpha', y='CL')
+f.add(so.Line(color='grey'))
+f.add(so.Dot(marker='o', color='seagreen'))
+f.show()
+(so.Plot(data=df, x=['alpha','alpha'], y=['Cm','CL'])
+ .add(so.Line())
+ .show()
+)
+#plt.plot(df.loc[:,'alpha'], df.loc[:,'CD'])
+'''
+if __name__=="__main__":
+    main()
--- 2D_heat.py
+++ 2D_heat.py
@@ -1,8 +1,17 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
+import numpy as np
 
-df = pd.read_csv('.\Measurement_Data\\2D_heat\\3 deg\Record_2022-11-29_09-31-06.csv',delimiter=';')
-ax = sns.heatmap(df,cmap='cubehelix')
-print(df)
+
+df = pd.read_csv('.\Measurement_Data\\2D_heat\\3 deg\Record_2022-11-29_09-31-06.csv',delimiter=';', skipinitialspace=True)
+df = df.iloc[: , :-1]
+ma = df.values.max()
+mi = df.values.min()
+df = df.apply(lambda x: (x-mi)/(ma-mi))
+df = df.apply(lambda x: np.tanh(2*(x-0.73)))
+a = sns.set_palette(sns.color_palette(['#000000','#ffffff'], as_cmap=True))
+a = sns.color_palette('Greys_r', 1000)
+ax = sns.heatmap(df,cmap=a, yticklabels=False, xticklabels=False)
+print(df.iloc[225,220])
 plt.show()
